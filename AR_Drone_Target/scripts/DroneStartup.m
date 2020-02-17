ef7eb1a9ef7dfcc2f5d5c6719492fc70ce34ffb6
Rafael Carbonell Lázaro <rafapenya96@gmail.com>
2020-02-17 17:39:51 +0100

    Update to R2019b compatible

--- AR_Drone_Target/scripts/DroneStartup.m
+++ AR_Drone_Target/scripts/DroneStartup.m
@@ -21,8 +21,8 @@ if isequal(currentFolder(end-6:end),'scripts')
 end
 
 %% Check if the compiler has been registered
-if exist(['AR_Drone_Target/registry/thirdpartytools/thirdpartytools_win32.xml'],'file') == 0 || ...
-        exist(['AR_Drone_Target/registry/thirdpartytools/thirdpartytools_win64.xml'],'file') == 0
+if isfile('AR_Drone_Target/registry/thirdpartytools/thirdpartytools_win32.xml') == 0 || ...
+        isfile('AR_Drone_Target/registry/thirdpartytools/thirdpartytools_win64.xml') == 0
     disp('No third party compiler has been registered for the AR Drone, running install script')
     cd AR_Drone_Target/scripts/
     run('install_script.m') 
@@ -30,12 +30,12 @@ if exist(['AR_Drone_Target/registry/thirdpartytools/thirdpartytools_win32.xml'],
 end
 
 %% Check if TargetRegistry has been set
-if exist(['AR_Drone_Target/registry/gcc_codesourcery_arm_linux_gnueabihf_gmake_win64_v4_8.mat'],'file') == 0
+if isfile('AR_Drone_Target/registry/gcc_codesourcery_arm_linux_gnueabihf_gmake_win64_v4_8.mat') == 0
     disp('No toolchain info mat file was found for use with rtwTargetInfo.m, generating the gcc_codesourcery_arm_linux_gnueabihf_gmake_win64 mat file')
     % cd to the folder where the mat file should be put
-    cd AR_Drone_Target/registry
-    run('../registry/generateTargetInfoMatFile.m');  % create the mat file containing the toolchain info
-    cd ../..
+    cd AR_Drone_Target
+    run('registry/generateTargetInfoMatFile.m');  % create the mat file containing the toolchain info
+    cd ..
     RTW.TargetRegistry.getInstance('reset');         % reset the TargetRegistry such that rtwTargetInfo.m is called when a model is made for the first time
 end    
 
@@ -43,8 +43,8 @@ end
 sl_refresh_customizations;
 
 %% Compile the video library if needed
-if exist(['AR_Drone_Target/blocks/videolib/AR_Drone_Front_Camera.tlc'],'file') == 0 ||...
-        exist(['AR_Drone_Target/blocks/videolib/AR_Drone_Bottom_Camera.tlc'],'file') == 0
+if isfile('AR_Drone_Target/blocks/videolib/AR_Drone_Front_Camera.tlc') == 0 ||...
+        isfile('AR_Drone_Target/blocks/videolib/AR_Drone_Bottom_Camera.tlc') == 0
     disp('No video source files were found, starting compilation of the video library using the Legacy Code Tool')
     cd AR_Drone_Target/blocks/videolib
     run('Generate_AR_Drone_Video.m')
@@ -53,7 +53,7 @@ if exist(['AR_Drone_Target/blocks/videolib/AR_Drone_Front_Camera.tlc'],'file') =
 end
 
 %% Create the s functions for the AR_Drone_Library blocks
-if exist(['AR_Drone_Target/blocks/rtwmakecfg.m'],'file') == 0 % the rtwmakecfg.m is created at the very end of Generate_AR_Drone_S_Functions
+if isfile('AR_Drone_Target/blocks/rtwmakecfg.m') == 0 % the rtwmakecfg.m is created at the very end of Generate_AR_Drone_S_Functions
     disp('No rtwmakecfg found for the blocks, running Generate_AR_Drone_S_Functions')
     cd AR_Drone_Target/blocks
    run('Generate_AR_Drone_S_Functions.m')
